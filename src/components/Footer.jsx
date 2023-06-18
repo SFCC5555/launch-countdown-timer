f8dc7914f5e32a129ac056bb662c7e11ece8f885
sfcc5555 <sfcc.5555@gmail.com>
2023-06-18 01:55:19 -0500

    application finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   const {theme} = useContext(AppContext) 
 
   return (
-    <div className={`flex items-center justify-center gap-10 w-full h-48`}  style={{backgroundImage:`url(${hills})`}}>
+    <div className={`flex items-end pb-10 justify-center gap-10 w-full h-48`}  style={{backgroundImage:`url(${hills})`}}>
       <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'><span className={`w-6 h-6 facebook${theme.status}`} /></a>
       <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'><span className={`w-6 h-6 instagram${theme.status}`} /></a>
       <a href='https://www.pinterest.com/' target='_blank' rel='noreferrer'><span className={`w-6 h-6 pinterest${theme.status}`} /></a>
