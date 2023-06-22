64569aba01657d38b2e4a1e34dd5aeda7d1e169d
sfcc5555 <sfcc.5555@gmail.com>
2023-06-22 03:16:21 -0500

    a

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,7 +4,7 @@ import stars from '../assets/images/bg-stars.svg'
 const Background = () => {
     
   return (
-        <div className={`w-full h-screen top-0 -z-10 absolute background`}>
+        <div className={`w-full h-full top-0 -z-10 absolute background`}>
             <div className='w-full h-full' style={{backgroundImage: `url(${stars})`}} />
         </div>
 
