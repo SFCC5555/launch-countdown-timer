82a2d25e83a15e05a2871126868d157ea8d85ae3
sfcc5555 <sfcc.5555@gmail.com>
2023-06-29 07:04:57 -0500

    a

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
-import './index.scss';
 import './index.css';
+import './index.scss';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
